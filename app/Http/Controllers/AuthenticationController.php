fec37aa8387934df13aa49703e0c0ad8362c32c9
lindstr_r <romeo.lindstrom06@outlook.com>
2025-05-30 13:41:14 +0300

    Implement logout method and route for user session handling – AuthenticationController.php, web.php

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -116,4 +116,14 @@ class AuthenticationController extends Controller
             'email' => 'The provided credentials are incorrect.',
         ])->onlyInput('email');
     }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login.show')->with('system', 'Successfully signed out');
+    }
 }
